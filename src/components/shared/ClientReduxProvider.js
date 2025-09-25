7a03b208f4bfa6d5b261d11dd2540e6a5050b7ce
Dalakoti01 <dalakotikaran11@gmail.com>
2025-09-26 02:10:19 +0530

    another commit

--- src/components/shared/ClientReduxProvider.js
+++ src/components/shared/ClientReduxProvider.js
@@ -1,10 +1,24 @@
+// components/shared/ReduxProvider.js
 "use client";
 
-import { persistor, store } from "@/redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { useEffect, useState } from "react";
+import { store } from "@/redux/store";
 
 export default function ClientReduxProvider({ children }) {
+  const [persistor, setPersistor] = useState(null);
+
+  useEffect(() => {
+    const p = persistStore(store);
+    setPersistor(p);
+  }, []);
+
+  if (!persistor) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
